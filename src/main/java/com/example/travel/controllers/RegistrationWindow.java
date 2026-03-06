2957ba100da2612146ff1dd29f54876be85fd266
AlexChernov-6 <Kapruz3003@yandex.ru>
2026-03-06 14:23:36 +0300

    Реализовано окошко подтверждения почты

--- src/main/java/com/example/travel/controllers/RegistrationWindow.java
+++ src/main/java/com/example/travel/controllers/RegistrationWindow.java
@@ -1,8 +1,10 @@
 package com.example.travel.controllers;
 
-import com.example.travel.services.DirectionService;
+import com.example.travel.TravelApplication;
 import com.example.travel.util.HelpFullClass;
 import com.example.travel.util.SendingClass;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
@@ -10,14 +12,17 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class RegistrationWindow extends AnchorPane {
@@ -36,7 +41,18 @@ public class RegistrationWindow extends AnchorPane {
 
     private Timer countdownTimer;
 
-    private Button getCodeBtn;
+    private Button getCodeBtn, getCodeBtnAgain;
+
+    private VBox emailVB, confirmEmailVB;
+
+    private List<TextField> enterNumbers;
+
+    private TextField emailTF;
+
+    private boolean updating = false;
+    private int secondsRemaining;
+
+    private Text boldEmail;
 
     public RegistrationWindow(StackPane overlaySP) {
         this.overlaySP = overlaySP;
@@ -52,9 +68,9 @@ public class RegistrationWindow extends AnchorPane {
         setStyle("-fx-background-color: white;");
         getStyleClass().add("popup");
 
-        createHeader();
+        createEnteredEmail();
 
-        createBody();
+        createConfirmEmail();
 
         overlaySP.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
             if (!isVisible()) return;
@@ -69,17 +85,6 @@ public class RegistrationWindow extends AnchorPane {
         overlaySP.getChildren().add(this);
     }
 
-    private void createHeader() {
-        Label headerLB = new Label("Войти или создать профиль");
-        headerLB.getStyleClass().add("filters-label");
-        headerLB.setAlignment(Pos.BOTTOM_CENTER);
-        AnchorPane.setTopAnchor(headerLB, 25.0);
-        AnchorPane.setLeftAnchor(headerLB, 15.0);
-        AnchorPane.setRightAnchor(headerLB, 15.0);
-
-        getChildren().add(headerLB);
-    }
-
     public void show() {
         shadowPane.setVisible(true);
         setVisible(true);
@@ -90,25 +95,30 @@ public class RegistrationWindow extends AnchorPane {
         setVisible(false);
     }
 
-    public void createBody() {
-        VBox emailVB = new VBox(2);
-        AnchorPane.setTopAnchor(emailVB, 90.0);
+    public void createEnteredEmail() {
+        emailVB = new VBox(2);
+        AnchorPane.setTopAnchor(emailVB, 25.0);
         AnchorPane.setLeftAnchor(emailVB, 50.0);
         AnchorPane.setRightAnchor(emailVB, 50.0);
         emailVB.setAlignment(Pos.TOP_CENTER);
+        emailVB.setOpacity(1.0);
+
+        Label headerLB = new Label("Войти или создать профиль");
+        headerLB.getStyleClass().add("filters-label");
 
         Label hintEmail = new Label("Адрес электронной почты");
         hintEmail.getStyleClass().add("hint-label-registration");
+        VBox.setMargin(hintEmail, new Insets(40, 0, 0, 0));
         hintEmail.setAlignment(Pos.BOTTOM_LEFT);
         hintEmail.prefWidthProperty().bind(emailVB.widthProperty().subtract(10));
         hintEmail.textProperty().addListener((ob, oldV, newV) -> {
             if(newV.equals("Адрес электронной почты"))
-                setStyle("");
+                hintEmail.setStyle("");
             else
-                setStyle("-fx-text-fill: rgba(130,0,0);");
+                hintEmail.setStyle("-fx-text-fill: rgba(130,0,0);");
         });
 
-        TextField emailTF = new TextField();
+        emailTF = new TextField();
         emailTF.setPromptText("Введите email");
         emailTF.getStyleClass().add("text-field-email");
         emailTF.textProperty().addListener((ob, oldV, newV) -> {
@@ -121,24 +131,36 @@ public class RegistrationWindow extends AnchorPane {
         getCodeBtn.prefWidthProperty().bind(emailTF.widthProperty());
         getCodeBtn.prefHeightProperty().bind(emailTF.heightProperty());
         getCodeBtn.setOnAction(e -> {
+            if (emailTF.getText().isEmpty()) {
+                hintEmail.setText("Укажите адрес электронной почты");
+                return;
+            }
+
             if (getCodeBtn.getText().equals("Получить код") && hintEmail.getText().equals("Адрес электронной почты")) {
                 String mail = emailTF.getText();
-                if (!SendingClass.canSendEmail(emailTF.getText())) {
+                boldEmail.setText(mail);
+
+                if (!SendingClass.canSendEmail(mail)) {
                     int remaining = SendingClass.getRemainingTime(mail);
-                    startCountdown(remaining);
+                    startCountdownIfNeeded(remaining);
                     return;
                 }
 
-                // ОТПРАВЛЯЕМ email
-                new Thread(() -> {
-                    boolean sent = SendingClass.sendPostalDelivery(mail);
-                    if(sent)
-                        System.out.println("Сообщение отправлено");
-                    else
-                        System.out.println("Сообщение не отправлено");
-                }).start();
+                FadeTransition fadeOut = new FadeTransition(Duration.millis(200), emailVB);
+                fadeOut.setToValue(0.0);
+                fadeOut.setOnFinished(event -> {
+                    emailVB.setManaged(false);
+                    emailVB.toBack();
+                    confirmEmailVB.setManaged(true);
+                    FadeTransition fadeIn = new FadeTransition(Duration.millis(600), confirmEmailVB);
+                    fadeIn.setToValue(1.0);
+                    fadeIn.play();
+                });
+                fadeOut.play();
 
-                startCountdown(60);
+                startCountdownIfNeeded(60);
+
+                // new Thread(() -> SendingClass.sendPostalDelivery(mail)).start();
             }
         });
 
@@ -150,17 +172,104 @@ public class RegistrationWindow extends AnchorPane {
         Button withTheRulesForUsingTheTradingPlatform = new Button("с правилами пользования торговой площадкой");
         withTheRulesForUsingTheTradingPlatform.getStyleClass().add("documents-button");
         withTheRulesForUsingTheTradingPlatform.setOnAction(e ->
-                HelpFullClass.openWebPage("https://metanit.com/java/javafx/3.2.php"));
+                HelpFullClass.openWebPage("https://zelmex.ru/"));
 
         Button privacyPolicy = new Button("Политика конфиденциальности");
         privacyPolicy.getStyleClass().add("documents-button");
         privacyPolicy.setOnAction(e ->
-                HelpFullClass.openWebPage("https://metanit.com/java/javafx/3.2.php"));
+                HelpFullClass.openWebPage("https://zelmex.ru/"));
 
 
-        emailVB.getChildren().addAll(hintEmail, emailTF, getCodeBtn, userAgreementLB, withTheRulesForUsingTheTradingPlatform, privacyPolicy);
+        emailVB.getChildren().addAll(headerLB, hintEmail, emailTF, getCodeBtn, userAgreementLB, withTheRulesForUsingTheTradingPlatform, privacyPolicy);
 
         getChildren().add(emailVB);
+    }
+
+    private void createConfirmEmail() {
+        enterNumbers = new ArrayList<>();
+
+        confirmEmailVB = new VBox(20);
+        AnchorPane.setTopAnchor(confirmEmailVB, 25.0);
+        AnchorPane.setLeftAnchor(confirmEmailVB, 50.0);
+        AnchorPane.setRightAnchor(confirmEmailVB, 50.0);
+        confirmEmailVB.setOpacity(0.0);
+        confirmEmailVB.setAlignment(Pos.TOP_CENTER);
+        confirmEmailVB.setManaged(false);
+
+        StackPane headerStackPane = new StackPane();
+        headerStackPane.prefHeight(30);
+
+        Label headerLB = new Label("Подтверждение email");
+        headerLB.getStyleClass().add("filters-label");
+
+        Button backBtn = new Button();
+        backBtn.getStyleClass().add("back-button-double");
+        backBtn.setPrefWidth(30);
+        backBtn.setPrefHeight(30);
+        backBtn.setOnAction(e -> {
+            FadeTransition fadeOut = new FadeTransition(Duration.millis(200), confirmEmailVB);
+            fadeOut.setToValue(0.0);
+            fadeOut.setOnFinished(event -> {
+                confirmEmailVB.setManaged(false);
+                emailVB.setManaged(true);
+                emailVB.toFront();
+                FadeTransition fadeIn = new FadeTransition(Duration.millis(600), emailVB);
+                fadeIn.setToValue(1.0);
+                fadeIn.play();
+            });
+            fadeOut.play();
+        });
+
+        ImageView imageView = new ImageView(
+                new Image(Objects.requireNonNull(TravelApplication.class.getResourceAsStream("/images/left-arrow.png"))));
+        imageView.setFitWidth(25 / 1.5);
+        imageView.setFitHeight(25);
+        imageView.setPreserveRatio(true);
+
+        backBtn.setGraphic(imageView);
+        StackPane.setAlignment(backBtn, Pos.CENTER_LEFT);
+
+        headerStackPane.getChildren().addAll(headerLB, backBtn);
+
+        TextFlow hintTextFlow = new TextFlow();
+
+        Text before = new Text("Мы отправили код на адрес: ");
+        before.setStyle("-fx-font-size: 18px;");
+        boldEmail = new Text();
+        boldEmail.setStyle("-fx-font-weight: bold; -fx-font-size: 18px;");
+        Text after = new Text(", пожалуйста, проверьте папки «Входящие» и «Спам»");
+        after.setStyle("-fx-font-size: 18px;");
+
+        hintTextFlow.getChildren().addAll(before, boldEmail, after);
+
+        HBox enteredHB = new HBox(10);
+        enteredHB.setAlignment(Pos.CENTER);
+
+        for(int i = 0; i < 6; i = i + 1) {
+            enteredHB.getChildren().add(createNumberCodeTF());
+        }
+
+        getCodeBtnAgain = new Button("Получить код");
+        getCodeBtnAgain.getStyleClass().add("show-result-button");
+        VBox.setMargin(getCodeBtnAgain, new Insets(10, 0, 0, 0));
+        getCodeBtnAgain.prefWidthProperty().bind(emailTF.widthProperty());
+        getCodeBtnAgain.prefHeightProperty().bind(emailTF.heightProperty());
+        getCodeBtnAgain.setOnAction(e -> {
+            if (getCodeBtnAgain.getText().equals("Получить код")) {
+                if (!SendingClass.canSendEmail(boldEmail.getText())) {
+                    int remaining = SendingClass.getRemainingTime(boldEmail.getText());
+                    startCountdownIfNeeded(remaining);
+                    return;
+                }
+                startCountdownIfNeeded(60);
+
+                // new Thread(() -> SendingClass.sendPostalDelivery(mail)).start();
+            }
+        });
+
+        confirmEmailVB.getChildren().addAll(headerStackPane, hintTextFlow, enteredHB, getCodeBtnAgain);
+
+        getChildren().add(confirmEmailVB);
     }
 
     private static String validateEmail(String email) {
@@ -217,26 +326,40 @@ public class RegistrationWindow extends AnchorPane {
         return null;
     }
 
-    private void startCountdown(int startSeconds) {
-        // Отменяем предыдущий таймер
+    private void updateButtonsText(int seconds) {
+        if (seconds > 0) {
+            String text = "Повторно через " + seconds;
+            getCodeBtn.setText(text);
+            getCodeBtnAgain.setText(text);
+            getCodeBtn.setDisable(true);
+            getCodeBtnAgain.setDisable(true);
+        } else {
+            getCodeBtn.setText("Получить код");
+            getCodeBtnAgain.setText("Получить код");
+            getCodeBtn.setDisable(false);
+            getCodeBtnAgain.setDisable(false);
+        }
+    }
+
+    private void startCountdownIfNeeded(int startSeconds) {
         if (countdownTimer != null) {
-            countdownTimer.cancel();
+            updateButtonsText(secondsRemaining);
+            return;
         }
 
-        getCodeBtn.setDisable(true);
-        final int[] secondsRemaining = {startSeconds};
+        secondsRemaining = startSeconds;
+        updateButtonsText(secondsRemaining);
 
         countdownTimer = new Timer();
         countdownTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                    secondsRemaining[0]--;
-                    if (secondsRemaining[0] > 0) {
-                        getCodeBtn.setText("Повторно через " + secondsRemaining[0]);
+                    secondsRemaining--;
+                    if (secondsRemaining > 0) {
+                        updateButtonsText(secondsRemaining);
                     } else {
-                        getCodeBtn.setText("Получить код");
-                        getCodeBtn.setDisable(false);
+                        updateButtonsText(0);
                         countdownTimer.cancel();
                         countdownTimer = null;
                     }
@@ -244,4 +367,141 @@ public class RegistrationWindow extends AnchorPane {
             }
         }, 1000, 1000);
     }
+
+    private TextField createNumberCodeTF() {
+        TextField number = new TextField();
+        number.setPrefWidth(40);
+        number.setPrefHeight(40);
+        number.setAlignment(Pos.CENTER);
+        number.getStyleClass().add("text-field-email");
+        number.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.isEmpty() || newText.matches("\\d")) {
+                return change;
+            } else {
+                String lastChar = newText.substring(newText.length() - 1);
+                if (lastChar.matches("\\d")) {
+                    change.setText(lastChar);
+                    change.setRange(0, change.getControlText().length());
+                    change.setCaretPosition(lastChar.length());
+                    change.setAnchor(lastChar.length());
+                    return change;
+                }
+            }
+            return null;
+        }));
+
+        enterNumbers.add(number);
+
+        number.textProperty().addListener((ob, oldV, newV) -> {
+            if (updating) return;
+            updating = true;
+            try {
+                if(getEnteredCode(enterNumbers).length() == 6) {
+                    System.out.println(getEnteredCode(enterNumbers));
+                    for(TextField tf : enterNumbers)
+                        tf.setText("");
+                    enterNumbers.getFirst().requestFocus();
+                    return;
+                }
+
+                if (!enterNumbers.getLast().equals(number) && !enterNumbers.getFirst().equals(number)) {
+                    TextField nextTF = null, prevTF = null;
+                    for (int i = 0; i < enterNumbers.size(); i++) {
+                        TextField currentTF = enterNumbers.get(i);
+                        if (currentTF.equals(number)) {
+                            nextTF = enterNumbers.get(i + 1);
+                            prevTF = enterNumbers.get(i - 1);
+                            break;
+                        }
+                    }
+
+                    if (newV.isEmpty()) {
+                        prevTF.requestFocus();
+                        prevTF.positionCaret(1);
+                        prevTF.cancelEdit();
+                    } else {
+                        nextTF.requestFocus();
+                        nextTF.positionCaret(1);
+                        nextTF.cancelEdit();
+                    }
+                } else if (enterNumbers.getLast().equals(number)) {
+                    TextField prevTF = enterNumbers.get(enterNumbers.size() - 2);
+
+                    if (newV.isEmpty()) {
+                        prevTF.requestFocus();
+                        prevTF.positionCaret(1);
+                        prevTF.cancelEdit();
+                    }
+
+
+                } else if (enterNumbers.getFirst().equals(number)) {
+                    TextField nextTF = enterNumbers.get(1);
+
+                    if (!newV.isEmpty()) {
+                        nextTF.requestFocus();
+                        nextTF.positionCaret(1);
+                        nextTF.cancelEdit();
+                    }
+                }
+            } finally {
+                updating = false;
+            }
+        });
+
+        number.setOnKeyPressed(e -> {
+            TextField nextTF = null, prevTF = null;
+            if (enterNumbers.getFirst().equals(number))
+                nextTF = enterNumbers.get(1);
+            else if (enterNumbers.getLast().equals(number))
+                prevTF = enterNumbers.get(enterNumbers.size() - 2);
+            else {
+                for (int i = 0; i < enterNumbers.size(); i++) {
+                    TextField currentTF = enterNumbers.get(i);
+                    if (currentTF.equals(number)) {
+                        nextTF = enterNumbers.get(i + 1);
+                        prevTF = enterNumbers.get(i - 1);
+                        break;
+                    }
+                }
+            }
+
+            if(e.getCode() == KeyCode.UP || e.getCode() == KeyCode.RIGHT) {
+                if(nextTF != null) {
+                    nextTF.requestFocus();
+                    nextTF.positionCaret(1);
+                    nextTF.cancelEdit();
+                }
+            } else if(e.getCode() == KeyCode.DOWN || e.getCode() == KeyCode.LEFT) {
+                if(prevTF != null) {
+                    prevTF.requestFocus();
+                    prevTF.positionCaret(1);
+                    prevTF.cancelEdit();
+                }
+            } else if(e.getCode() == KeyCode.BACK_SPACE && number.getText().isEmpty()) {
+                if(prevTF != null) {
+                    prevTF.requestFocus();
+                    prevTF.positionCaret(1);
+                    prevTF.cancelEdit();
+                }
+            } else if(e.getCode() == KeyCode.SPACE) {
+                if(nextTF != null) {
+                    nextTF.requestFocus();
+                    nextTF.positionCaret(1);
+                    nextTF.cancelEdit();
+                }
+            }
+        });
+
+        return number;
+    }
+
+    private static String getEnteredCode(List<TextField> textFields) {
+        StringBuilder sB = new StringBuilder();
+        for (TextField tF : textFields) {
+            sB.append(tF.getText());
+        }
+        return sB.toString();
+    }
+
 }
